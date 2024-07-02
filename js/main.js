e88d24c36bc32ac07df4b5fcf3721729c2181615
NairaHakobyan666 <122293119+NairaHakobyan666@users.noreply.github.com>
2024-07-02 15:19:13 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+   var mixer = mixitup('.filter__inner'); 
+});
+
+const swiper = new Swiper('.swiper', {
+   loop: true,
+   
+ 
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   },
+
+ });
+
+
+
+
+
